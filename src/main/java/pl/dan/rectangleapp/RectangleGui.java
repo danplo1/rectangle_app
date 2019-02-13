7e676fc25f6875f749d2b877024466e40682151d
danplo1 <budweiser401@wp.pl>
2019-02-13 20:16:24 +0100

    Spring Boot and Vaadin - example of API and GUI

--- src/main/java/pl/dan/rectangleapp/RectangleGui.java
+++ src/main/java/pl/dan/rectangleapp/RectangleGui.java
@@ -3,20 +3,44 @@ package pl.dan.rectangleapp;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.awt.*;
 
-@Route  // dostęp do biblioteki Vaadim
-//klasa która bedzie nam się wyswoetlała w przeglądarce, bedzie zawierać komponenty z biblioteki Vaadim
+@Route // dostęp do biblioteki Vaadim
 public class RectangleGui extends VerticalLayout {
 
+//klasa która bedzie nam się wyswoetlała w przeglądarce, bedzie zawierać komponenty z biblioteki Vaadim
+
+    private RectangleRepo rectangleRepo;
 
     private TextField textFieldHeight;
     private TextField textFieldWidth;
     private Button button;
 
-    public RectangleGui() {
-        
+    @Autowired
+    public RectangleGui(RectangleRepo rectangleRepo) { // inicjalizacja pól w konstruktorze
+
+        this.rectangleRepo = rectangleRepo; //wstrzykiwanie rectangleRepo
+        textFieldHeight = new TextField("Podaj wysokość: ");
+        textFieldWidth = new TextField("Podaj szerokość: ");
+        button = new Button(" Dodaj!");
+
+        button.addClickListener(buttonClickEvent -> addRectangle());
+
+        add(textFieldHeight);
+        add(textFieldWidth);
+        add(button);
+
+    }
+
+
+    public void addRectangle() {
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setHeight(Integer.parseInt(textFieldHeight.getValue()));
+        rectangle.setWidth(Integer.parseInt(textFieldWidth.getValue()));
+        rectangleRepo.save(rectangle);
     }
 }
